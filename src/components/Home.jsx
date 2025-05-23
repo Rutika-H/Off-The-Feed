3849a51673df09dc5886424794d0fc70adbd79fd
Rutika Hake <rutikahake2005@gmail.com>
2025-05-23 20:14:46 +0530

    Homepage intro para

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,7 +8,20 @@ function Home(){
       </video>
 
       <div >
-        <h1 className="home-content">Welcome to Off The Feed!!!</h1>
+        <h1 className="home-content">Welcome to Off The Feed!💟</h1>
+        <p className="about-me">👾You’ve just tuned into Off The Feed-curated by me.
+            <br/>
+          I’m Rutika🙋🏻‍♀️, and this is the calm corner of the internet.
+          <br/>
+          Welcome to my space,
+          <br/>
+          where i build cool things without the noise!
+          <br/>
+          ❤️🐻‍❄️🌼🏡⚡
+        </p>
+       <a href="https://www.instagram.com/rutikahake/" target="_blank" >
+       <h4 className="socials">Click this to follow me on Instagram!❤️ </h4>
+      </a>
     </div>
      
        </div>
