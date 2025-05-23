175be17d0ffc51f6f55cf12eb73ba17b85e1801d
Rutika Hake <rutikahake2005@gmail.com>
2025-05-23 18:58:02 +0530

    corrected path

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -5,35 +5,35 @@ import "../css/MovieCard.css"
 function Movies(){
     const movies=[
         {id:1, title:"Your Name", release_date:"2016",
-            url:"/your-name-final.jpg",
+            url:"/covers/your-name-final.jpg",
             ref:"https://www.imdb.com/title/tt5311514/"
         },
         {id:2, title:"Interstellar", release_date:"2014",
-            url:"/interstellar.webp",
+            url:"/covers/interstellar.webp",
             ref:"https://www.imdb.com/title/tt0816692/"
         },
         {id:3, title:"Weathering with you", release_date:"2019",
-            url:"/wwu.jpg",
+            url:"/covers/wwu.jpg",
             ref:"https://www.imdb.com/title/tt9426210/"
         },
         {id:4, title:"Free Guy", release_date:"2021",
-            url:"/free-guy final.jpg",
+            url:"/covers/free-guy final.jpg",
             ref:"https://www.imdb.com/title/tt6264654/"
         },
         {id:5, title:"Inception", release_date:"2010",
-            url:"/inception.jpg",
+            url:"/covers/inception.jpg",
             ref:"https://www.imdb.com/title/tt1375666/"
         },
         {id:6, title:"Oppenheimer", release_date:"2023",
-            url:"/oppenheimer final.webp",
+            url:"/covers/oppenheimer final.webp",
             ref:"https://www.imdb.com/title/tt15398776/"
         },
          {id:7, title:"Demon Slayer:Mugen Train", release_date:"2019",
-            url:"/mugen-train.webp",
+            url:"/covers/mugen-train.webp",
             ref:"https://www.imdb.com/title/tt11032374/"
         },
         {id:8, title:"Harry Potter and the Deathly Hallows Part-2", release_date:"2011",
-            url:"/harry-potter.jpg",
+            url:"/covers/harry-potter.jpg",
             ref:"https://www.imdb.com/title/tt1201607/"
         }
     ]
